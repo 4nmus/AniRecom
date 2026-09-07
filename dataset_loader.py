24368d3fe48d4e9820a3b22b474dd6496709ab20
4nmus <145120721+4nmus@users.noreply.github.com>
2026-09-07 19:07:29 +0200

    Added multi lable encoding

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,11 +1,11 @@
 import sys
-
 import pandas as pd
 import numpy as np
 import os
 import logging
+from sklearn.preprocessing import MultiLabelBinarizer
 
-#Loggers
+# Loggers
 file_handler = logging.FileHandler("logs.log")
 file_handler.setLevel(logging.DEBUG)
 
@@ -19,7 +19,9 @@ logging.basicConfig(format='[%(asctime)s - %(name)s: %(levelname)s - %(message)s
     ],
 )
 
-#Daa
+# Dataset functions
+
+
 def load_available_dataset() -> pd.DataFrame:
     try:
         path = os.getcwd()
@@ -32,10 +34,23 @@ def load_available_dataset() -> pd.DataFrame:
                 break
         return df
 
-    except:
+    except Exception:
         logging.error(f"No available csv found. It shall be in {os.getcwd()}/datasets")
 
 
+def separate_combined_feature(df: pd.DataFrame, column:str, split_sign: str) -> pd.DataFrame:
+    # Used to separate combines features. Example df['genres'] = Fantasy; Drama -> df['fantasy'] = 1 , df['drama'] = 1
+    separaed_items = df[column].fillna('').str.split(f'{split_sign}')
+    mlb = MultiLabelBinarizer()
+    encoded = mlb.fit_transform(separaed_items)
+    df_genre = pd.DataFrame(
+        encoded,
+        columns=mlb.classes_,
+        index=df.index
+    )
+
+    df = pd.concat([df, df_genre], axis=1)
+    return df
 
 
 if __name__ == "__main__":
