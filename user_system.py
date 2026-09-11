34a501dee4469d0254e45c809b6396c7aa185b16
4nmus <145120721+4nmus@users.noreply.github.com>
2026-09-11 16:02:29 +0200

    Added liked selector

---
+++ user_system.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+import pandas as pd
+import os
+
+
+
+def select_liked(df: pd.DataFrame):
+
+    likely_seen = df[df['score'] >= 9]
+    for idx, row in likely_seen.head(100).iterrows():
+        print("DId you watch ? ")
+        print(row['english_title'])
+        ans = input("Liked it? (yes/no/exit): ").strip().lower()
+        if ans == "yes":
+            df.loc[idx, 'liked'] = 0
+        elif ans == "no":
+            df.loc[idx, 'liked'] = 1
+        elif ans == "exit":
+            break
+    return df
